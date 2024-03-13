e3e9d33f8ff52068d2b3899a88943eee57e9802c
Klim <admin@nwolfhub.org>
2024-03-13 11:06:19 +0300

    Start making loginHandler

--- app/src/main/java/org/nwolfhub/notes/LoginActivity.kt
+++ app/src/main/java/org/nwolfhub/notes/LoginActivity.kt
@@ -1,11 +1,14 @@
 package org.nwolfhub.notes
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.animation.LinearInterpolator
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -16,10 +19,15 @@ import org.nwolfhub.notes.util.WebWorker
 
 
 class LoginActivity : AppCompatActivity() {
+    companion object {
+        var context:Context? = null;
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        context = this;
 
+        //initialize back button listener
         onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 AlertDialog.Builder(this@LoginActivity)
@@ -37,10 +45,13 @@ class LoginActivity : AppCompatActivity() {
                     }.show()
             }
         })
+
+        //initialize all the stuff
         val pref = getSharedPreferences("web_updated", MODE_PRIVATE)
         val storage = ServerStorage(getSharedPreferences("web_updated", MODE_PRIVATE))
         val svInfo = storage.activeServer
         Log.d("Active server", svInfo?.address ?: "null")
+        //check if any server is active
         if (svInfo == null) {
             startActivity(Intent(this, ServerSelect::class.java))
             finish()
@@ -56,6 +67,30 @@ class LoginActivity : AppCompatActivity() {
                 }
             }.start()
         }
+
+
+    }
+    fun startCircle() {
+        val loader:ProgressBar = findViewById(R.id.afterLoginLoader)
+        val webview:WebView = findViewById(R.id.loginWebView)
+        loader.animate().apply {
+            interpolator = LinearInterpolator()
+            duration = 500
+            alpha(1f)
+            start()
+        }
+        webview.animate().apply {
+            interpolator = LinearInterpolator()
+            duration = 500
+            alpha(0f)
+            start()
+        }
+
+    }
+
+    override fun finish() {
+        context = null
+        super.finish()
     }
 
     class MyWebViewClient(val verifier: String) : WebViewClient() {
@@ -70,9 +105,11 @@ class LoginActivity : AppCompatActivity() {
 
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
+            Log.d("Url change detected", url.toString())
             if(url!!.contains("/postlogin")) {
                 val code = url.split("&code=")[1].split("&")[0]
                 Log.d("Keycloak code", code)
+
             }
         }
     }
