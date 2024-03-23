469ec9526738121b151364eb3c5eb4f724cffbd6
s0m31 <admin@nwolfhub.org>
2024-03-23 13:00:22 +0300

    Edit.kt update

--- app/src/main/java/org/nwolfhub/notes/Edit.kt
+++ app/src/main/java/org/nwolfhub/notes/Edit.kt
@@ -1,9 +1,14 @@
 package org.nwolfhub.notes
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AlertDialog
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -13,6 +18,7 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -27,16 +33,27 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import com.google.gson.Gson
 import org.nwolfhub.notes.model.Note
 import org.nwolfhub.notes.ui.theme.NotesTheme
+import org.nwolfhub.notes.util.NotesStorage
 
 class Edit : ComponentActivity() {
+    val activeNotePref = getSharedPreferences("active_note", MODE_PRIVATE)
+    val cachePref = getSharedPreferences("cached_notes", MODE_PRIVATE)
+    lateinit var note: Note
+    var noteText = ""
+    val showDialog = mutableListOf(false)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val activeNotePref = getSharedPreferences("active_note", MODE_PRIVATE)
-        val note = Gson().fromJson(activeNotePref.getString("active", null), Note::class.java)
+        note = Gson().fromJson(activeNotePref.getString("active", null), Note::class.java)
+        val prev = cachePref.getString(note.getServerAddr() + note.getOwner().getId() + note.getId(), null)
+        var prevText = ""
+        if(prev!=null) {
+            prevText = prev
+        }
         setContent {
             NotesTheme {
                 Surface(
@@ -45,7 +62,7 @@ class Edit : ComponentActivity() {
                 ) {
                     Column {
                         ActionsPanel(noteName = note.getName())
-                        mainEditField()
+                        MainEditField(prevText)
                     }
                 }
             }
@@ -57,28 +74,32 @@ class Edit : ComponentActivity() {
         Row(
             Modifier
                 .background(Color.Black)
-                .wrapContentHeight().fillMaxWidth()) {
-            TextButton(onClick = { /*TODO*/ }, Modifier.wrapContentWidth()) {
+                .wrapContentHeight()
+                .fillMaxWidth()) {
+            TextButton(onClick = {Toast.makeText(this@Edit, "Я обязательно сделаю эту кнопку...", Toast.LENGTH_LONG).show()
+                                 startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://cs11.pikabu.ru/post_img/2020/07/16/6/og_og_15948909712400651.jpg")))},
+                Modifier.wrapContentWidth()) {
                 Icon(Icons.Default.Settings, contentDescription = "Localized description")
             }
-            Text(text = noteName, color = Color.White, modifier = Modifier
+            Text(text = noteName, fontSize = 15.sp, textAlign = TextAlign.Center, color = Color.White, modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth())
         }
     }
 
     @Composable
-    @Preview
-    fun mainEditField():String {
-        var text by rememberSaveable { mutableStateOf("Text") }
+    fun MainEditField(previous: String) {
+        var text by rememberSaveable { mutableStateOf(previous) }
         Column(){
             Row(
                 Modifier
                     .weight(1f, true)) {
                 TextField(
-                    value = "",
+                    value = text,
                     onValueChange = {
                         text = it
+                        noteText = it
+                        cacheText(text)
                     },
                     placeholder = {
                         Text("And so it begins")
@@ -92,7 +113,9 @@ class Edit : ComponentActivity() {
                 Modifier
                     .wrapContentHeight()) {
                 Button(
-                    onClick = {},
+                    onClick = {
+                        val notesStorage = NotesStorage(getSharedPreferences("notes_updated", MODE_PRIVATE), getSharedPreferences("sync", MODE_PRIVATE))
+                    },
                     modifier = Modifier.fillMaxWidth()
 
                 ) {
@@ -100,8 +123,25 @@ class Edit : ComponentActivity() {
                 }
             }
         }
-        return text
     }
 
+    private fun cacheText(text: String) {
+        cachePref.edit().putString(note.getServerAddr() + note.getOwner().getId() + note.getId(), text).apply()
+    }
+    @Composable
+    fun AlertDialogFactory(
+        title:String,
+        content:String?,
+        button: @Composable () -> Unit
+    ) {
+        AlertDialog(onDismissRequest = {},
+            confirmButton = {button},
+            title = { Text(text = title) },
+            text = {
+                if (content!=null)
+                Text(text = content)
+                else {}
+            })
+    }
 
 }
